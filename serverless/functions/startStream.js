195a000b545a8d038ca92e832212ae369f351f66
Bhavith <catchbhav2002@gmail.com>
2022-09-04 06:41:33 -0400

    first commit

--- serverless/functions/startStream.js
+++ serverless/functions/startStream.js
@@ -1,66 +1,75 @@
-const twilio = require('twilio');
+const twilio = require("twilio");
 
-exports.handler = async function(context, event, callback) {
+console.log("1");
+exports.handler = async function (context, event, callback) {
+  console.log("2");
   if (!event.streamName) {
     const response = new twilio.Response();
     response.setStatusCode(401);
     response.setBody({
-      message: 'Missing stream name',
+      message: "Missing stream name",
     });
     return callback(null, response);
   }
-
+  console.log("3");
   if (!event.username) {
     const response = new twilio.Response();
     response.setStatusCode(401);
     response.setBody({
-      message: 'Missing user identity',
+      message: "Missing user identity",
     });
     return callback(null, response);
   }
-
+  console.log("4");
   const twilioClient = context.getTwilioClient();
 
   // Create the video room, player streamer, and audio mixer
   let room;
+  console.log("5");
   try {
     room = await twilioClient.video.rooms.create({
       uniqueName: event.streamName,
       audioOnly: true,
-      type: 'group'
+      type: "group",
     });
-
+    console.log("6");
   } catch (error) {
+    console.log("7");
     const response = new twilio.Response();
     response.setStatusCode(400);
     response.setBody({
-      message: 'Cannot create room',
-      error: error
+      message: "Cannot create room",
+      error: error,
     });
     return callback(null, response);
   }
-
-  const playerStreamer = await twilioClient.media.playerStreamer.create({video: false});
-
+  console.log("8");
+  const playerStreamer = await twilioClient.media.playerStreamer.create({
+    video: false,
+  });
+  console.log("9");
   const mediaProcessor = await twilioClient.media.mediaProcessor.create({
-    extension: 'audio-mixer-v1',
+    extension: "audio-mixer-v1",
     extensionContext: JSON.stringify({
-      identity: 'audio-mixer-v1',
+      identity: "audio-mixer-v1",
       room: {
-        name: room.sid
+        name: room.sid,
       },
-      outputs: [
-        playerStreamer.sid
-      ],
-    })
+      outputs: [playerStreamer.sid],
+    }),
   });
 
+  console.log("10");
   // Create an access token
-  const token = new twilio.jwt.AccessToken(context.ACCOUNT_SID, context.API_KEY_SID, context.API_KEY_SECRET);
+  const token = new twilio.jwt.AccessToken(
+    context.ACCOUNT_SID,
+    context.API_KEY_SID,
+    context.API_KEY_SECRET
+  );
 
   // Create a video grant
   const videoGrant = new twilio.jwt.AccessToken.VideoGrant({
-    room: event.streamName
+    room: event.streamName,
   });
 
   // Add the video grant and the user's identity to the token
@@ -68,12 +77,12 @@ exports.handler = async function(context, event, callback) {
   token.identity = event.username;
 
   return callback(null, {
-    streamDetails : {
+    streamDetails: {
       roomId: room.sid,
       streamName: event.streamName,
       playerStreamerId: playerStreamer.sid,
-      mediaProcessorId: mediaProcessor.sid
+      mediaProcessorId: mediaProcessor.sid,
     },
-    token: token.toJwt()
+    token: token.toJwt(),
   });
-}
+};
--- src/ManageStream.js
+++ src/ManageStream.js
@@ -1,19 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import { Box } from '@twilio-paste/core/box';
-import { Button } from '@twilio-paste/core/button';
-import { Input } from '@twilio-paste/core/input';
-import { Flex } from '@twilio-paste/core/flex';
-import { Heading } from '@twilio-paste/heading';
-import { Card } from '@twilio-paste/core/card';
-import { Column, Grid } from '@twilio-paste/core/grid';
-import { Label } from '@twilio-paste/core/label';
-import { Badge } from '@twilio-paste/core/badge';
+import { Box } from "@twilio-paste/core/box";
+import { Button } from "@twilio-paste/core/button";
+import { Input } from "@twilio-paste/core/input";
+import { Flex } from "@twilio-paste/core/flex";
+import { Heading } from "@twilio-paste/heading";
+import { Card } from "@twilio-paste/core/card";
+import { Column, Grid } from "@twilio-paste/core/grid";
+import { Label } from "@twilio-paste/core/label";
+import { Badge } from "@twilio-paste/core/badge";
 
-import { connect } from 'twilio-video';
-import VideoRoom from './VideoRoom';
+import { connect } from "twilio-video";
+import VideoRoom from "./VideoRoom";
 
-const ManageStream = ({username, setError}) => {
+const ManageStream = ({ username, setError }) => {
   const [room, setRoom] = useState(null);
   const [streamDetails, setStreamDetails] = useState(null);
   const [isLive, setIsLive] = useState(false);
@@ -29,17 +29,18 @@ const ManageStream = ({username, setError}) => {
     setIsLoading(true);
 
     try {
-      const response = await fetch('/startStream', {
-        method: 'POST',
+      console.log("START OF CODE");
+      const response = await fetch("/startStream", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           streamName: streamName,
-          username: username
-        })
+          username: username,
+        }),
       });
-
+      console.log("END OF CODE");
       const result = await response.json();
 
       // Get the details back from the server and the token for the video room
@@ -48,18 +49,17 @@ const ManageStream = ({username, setError}) => {
       // Join the video room with audio only
       const videoRoom = await connect(result.token, {
         name: result.streamDetails.roomId,
-        audio: true
+        audio: true,
       });
 
       setRoom(videoRoom);
       setIsLive(true);
       setIsLoading(false);
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
       setError(error);
     }
-  }
+  };
 
   const endStream = async (event) => {
     event.preventDefault();
@@ -70,66 +70,79 @@ const ManageStream = ({username, setError}) => {
 
     // Call to the backend to end all the resources and stop the stream
     try {
-      const response = await fetch('/endStream', {
-        method: 'POST',
+      const response = await fetch("/endStream", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          streamDetails: streamDetails
-        })
+          streamDetails: streamDetails,
+        }),
       });
 
       const result = await response.json();
       setIsLive(false);
       setStreamDetails(null);
       setIsLoading(false);
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
       setError(error);
     }
   };
 
-
   return (
-    <Card padding='space70'>
-      <Grid gutter='space30'>
+    <Card padding="space70">
+      <Grid gutter="space30">
         <Column span={8}>
-          {!isLive &&
+          {!isLive && (
             <Box>
               <form onSubmit={startStream}>
-                <Heading as='h4' variant='heading30'>Create a Stream</Heading>
-                <Label htmlFor='streamName' required>Name your stream</Label>
-                  <Input
-                    aria-describedby='stream_name_help_text'
-                    id='streamName'
-                    name='streamName'
-                    type='text'
-                    placeholder='My first livestream'
-                    required/>
-                  <Button variant='primary' type='submit' disabled={isLoading}>Start Stream</Button>
+                <Heading as="h4" variant="heading30">
+                  Create a Stream
+                </Heading>
+                <Label htmlFor="streamName" required>
+                  Name your stream
+                </Label>
+                <Input
+                  aria-describedby="stream_name_help_text"
+                  id="streamName"
+                  name="streamName"
+                  type="text"
+                  placeholder="My first livestream"
+                  required
+                />
+                <Button variant="primary" type="submit" disabled={isLoading}>
+                  Start Stream
+                </Button>
               </form>
             </Box>
-          }
-          {isLive &&
+          )}
+          {isLive && (
             <Card>
-              <Flex display='inline-flex'>
-              <Heading as='h4' variant='heading30'>
-                {streamDetails.streamName}
-                <Badge variant='default' as='span'>LIVE</Badge>
-              </Heading>
+              <Flex display="inline-flex">
+                <Heading as="h4" variant="heading30">
+                  {streamDetails.streamName}
+                  <Badge variant="default" as="span">
+                    LIVE
+                  </Badge>
+                </Heading>
               </Flex>
               <VideoRoom streamDetails={streamDetails} room={room} />
               <form onSubmit={endStream}>
-                <Button variant='destructive' type='submit' disabled={isLoading}>End Stream</Button>
+                <Button
+                  variant="destructive"
+                  type="submit"
+                  disabled={isLoading}
+                >
+                  End Stream
+                </Button>
               </form>
             </Card>
-          }
+          )}
         </Column>
       </Grid>
     </Card>
-  )
-}
+  );
+};
 
 export default ManageStream;
